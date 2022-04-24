8b0fa1b9288006e38b0956581840887b94030609
abi <salcedo.abigail.epet20@gmail.com>
2022-04-24 05:31:46 -0300

    listo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 const root = document.getElementById("titulo");
 root.innerHTML = "Programacion Web Dinamica"
 
+const root2 = document.getElementById("subtitulo");
+root2.innerHTML = "Trabajo Practico Tres"
+
+const root3 = document.getElementById("parrafo");
+root3.innerHTML = "me salió bien el js ;)"
